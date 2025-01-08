f015193c598bb6f9115408d73c0956e458397b8b
viswamraju <bogaraju@netapp.com>
2025-01-08 23:27:07 +0530

    added code

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,6 @@
 def call(String repoUrl, String branch){
     def workingDir = "${env.WORKSPACE}/rand"
-    sh "rm -rf ${workingDir}
+    sh "rm -rf ${workingDir}"
     sh "mkdir -p ${workingDir}"
     sh "git clone ${repoUrl} ${workingDir}"
     sh "git checkout ${branch}"
