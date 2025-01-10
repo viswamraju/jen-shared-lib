ed609130b0c7d40a7e068d32bc5acdf75aa93a93
viswamraju <bogaraju@netapp.com>
2025-01-10 15:17:19 +0530

    rublish results

--- vars/captureRobotResults.groovy
+++ vars/captureRobotResults.groovy
@@ -2,17 +2,38 @@ def call(){
 
     def build = currentBuild.build()
     def actions = build.actions
+
+    def tableData = [
+        ['Suite Name', 'Passed', 'Failed', 'Total', 'Pass %']
+    ]
                         
     actions.each() { action ->
         if( action.class.simpleName.equals("RobotBuildAction") ) { 
-
-            def passCount = action.getTotalCount() - action.getFailCount()
-            println "Suite Name: ${action.getxAxisLabel()}"
-            println "TotalCount: ${action.getTotalCount()}"
-            println "FailCount: ${action.getFailCount()}"
-            println "PassCount: ${passCount}"
-            println "PassPercentage: ${action.getOverallPassPercentage()}"
+            def row = [
+                action.getxAxisLabel(),
+                action.getTotalCount() - action.getFailCount(),
+                action.getFailCount(),
+                action.getTotalCount(),
+                action.getOverallPassPercentage()
+            ]
+            tableData.add(row)
         }
     }
-    
+
+    if (tableData.size() == 1) {
+        tableData.add(['No Robot Framework test results found.', '', '', '', ''])
+    } 
+
+    def tableString = formatTableData(tableData)
+    println tableString
+    return tableString
+}
+
+
+def formatTableData(tableData) {
+    def tableString = ''
+    tableData.each { row ->
+        tableString += row.join(' | ') + '\n'
+    }
+    return tableString
 }
